fff73a0ec00072352c2ad95ee9db8caa29a73626
gouyeonch <gouyeonch@naver.com>
2021-11-07 23:05:53 +0900

    에러 페이지 구현

--- main.js
+++ main.js
@@ -5,37 +5,38 @@ let url = require('url');
 let app = http.createServer(function(request, response) {
   let _url = request.url;
   let queryData = url.parse(_url, true).query; //url에서 쿼리 스트링만 추출
-  console.log(queryData.id);
+  let pathname = url.parse(_url, true).pathname;
   let title = queryData.id;
-  if(_url == '/') {
-    title = 'Welcome';
-  }
-  if(_url == '/favicon.ico') {
-    return response.writeHead(404);
-  }
 
-  response.writeHead(200);
-  fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-    let templete = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-    `;
-    response.end(templete);
-  })
+  if(pathname === '/')
+  {
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+      let templete = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+      response.writeHead(200);
+      response.end(templete);
+    })
+  }
+  else{
+    response.writeHead(404);
+    response.end('Not found');
+  }
 });
 app.listen(3000);
