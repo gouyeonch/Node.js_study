b7902b58fede011fc5fa1d178c8e58af6e69361e
gouyeonch <gouyeonch@naver.com>
2021-11-08 22:37:46 +0900

    함수로 코드 정리

--- main.js
+++ main.js
@@ -2,6 +2,36 @@ let http = require('http');
 let fs = require('fs');
 let url = require('url');
 
+function templeteHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    <h2>${title}</h2>
+    <p>${body}</p>
+  </body>
+  </html>
+  `;
+}
+
+function templeteList(filelist){
+  let list = '<ul>';
+  let i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i+=1;
+  }
+  list = list + '</ul>';
+
+  return list;
+}
+
 let app = http.createServer(function(request, response) {
   let _url = request.url;
   let queryData = url.parse(_url, true).query; //url에서 쿼리 스트링만 추출
@@ -12,58 +42,18 @@ let app = http.createServer(function(request, response) {
       fs.readdir('./data',function(error, filelist){
         let title = 'Welcome';
         let description = 'Hello, Node.js';
-        let list = '<ul>';
-        let i = 0;
-        while(i < filelist.length){
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-          i+=1;
-        }
-        list = list + '</ul>';
-        let templete = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
+        let list = templeteList(filelist);
+        let templete = templeteHTML(title, list, description);
         response.writeHead(200);
         response.end(templete);
       });
     }
     else{
       fs.readdir('./data',function(error, filelist){
-        let list = '<ul>';
-        let i = 0;
-        while(i < filelist.length){
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-          i+=1;
-        }
-        list = list + '</ul>';
+        let list = templeteList(filelist);
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
           let title = queryData.id;
-          let templete = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+          let templete = templeteHTML(title, list, description);
           response.writeHead(200);
           response.end(templete);
         });
